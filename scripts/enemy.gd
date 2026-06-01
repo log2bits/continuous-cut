2d229929d7da31663de84bc4a5de7f92f87cd16a
JacktheSeal <134016721+JacktheSeal@users.noreply.github.com>
2026-05-31 18:43:07 -0700

    Added Health Logic
    Added ability for player to take damage from enemies and die.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var speed: float = 32.0
+@export var damage: int = 1
+
 var player: CharacterBody2D
 
 func _physics_process(delta: float) -> void:
@@ -12,4 +14,6 @@ func _physics_process(delta: float) -> void:
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider() == player:
+			player.take_damage(damage)
+			
 			queue_free()
