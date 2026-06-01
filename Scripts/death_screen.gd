40159131ae0f66eb41e3bb7a1e70a5260ffdee39
JacktheSeal <134016721+JacktheSeal@users.noreply.github.com>
2026-05-31 18:10:21 -0700

    Added Music
    Added background music to title screen, main game, and death screen.

--- Scripts/death_screen.gd
+++ Scripts/death_screen.gd
@@ -1,14 +1,16 @@
 extends Control
 
+@onready var audio = $click_player
+
 func _on_quit_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
-	await $AudioStreamPlayer2D.finished
+	$click_player.play()
+	await $click_player.finished
 	get_tree().quit()
 	pass # Replace with function body.
 
 
 func _on_restart_button_pressed() -> void:
-	$AudioStreamPlayer2D.play()
-	await $AudioStreamPlayer2D.finished
+	$click_player.play()
+	await $click_player.finished
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 	pass # Replace with function body.
