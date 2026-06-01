9ccd90573f5fd252308342562ad0e0e1677e1ddf
JacktheSeal <134016721+JacktheSeal@users.noreply.github.com>
2026-05-31 17:30:22 -0700

    Added Click SFX

--- Scripts/death_screen.gd
+++ Scripts/death_screen.gd
@@ -1,10 +1,14 @@
 extends Control
 
 func _on_quit_button_pressed() -> void:
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_tree().quit()
 	pass # Replace with function body.
 
 
 func _on_restart_button_pressed() -> void:
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 	pass # Replace with function body.
--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,11 +1,16 @@
 extends Control
 
+@onready var audio = $AudioStreamPlayer2D
 
 func _on_start_button_pressed() -> void:
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 	pass # Replace with function body.
 
 
 func _on_quit_button_pressed() -> void:
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_tree().quit()
 	pass # Replace with function body.
